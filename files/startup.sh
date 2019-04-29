b83d98225ac0ebfadd9c39f8b66437d581860bdc
Adam Bowen <adam.bowen@delphix.com>
2019-04-29 17:02:10 -0400

    init

---
+++ files/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Copyright (c) 2011, 2019 by Delphix. All rights reserved.
+#
+# This file is managed by Ansible. Don't make changes here - they will be overwritten.
+ansible-playbook /etc/ansible/configure_hosts.yml  --limit localhost &>> startup.log
