cd44c69e0d8f1ce9fa5b916181abfc15ae595426
Adam Bowen <adam.bowen@delphix.com>
2019-06-26 17:10:49 -0400

    update script path

--- files/startup.sh
+++ files/startup.sh
@@ -3,4 +3,5 @@
 # Copyright (c) 2011, 2019 by Delphix. All rights reserved.
 #
 # This file is managed by Ansible. Don't make changes here - they will be overwritten.
-/usr/local/bin/ansible-playbook /etc/ansible/configure_hosts.yml  --limit localhost &>> startup.log
+export PATH=/usr/local/bin:${PATH}
+ansible-playbook /etc/ansible/configure_hosts.yml  --limit localhost &>> startup.log
